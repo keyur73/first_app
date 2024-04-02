9f321ce5ecd5361e776e1b4dd0cf6274d9e2b2a4
keyur73 <keyurkhambhadiya73@gmail.com>
2024-04-02 15:48:46 +0530

    final

--- first app/ContentView.swift
+++ first app/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var total = ""
+    @State var tippersent = 15.0
     var body: some View {
         VStack {
-            Text("project updated")
+            HStack {
+                Image (systemName: "rupeesign.circle.fill")
+                    .imageScale(.large)
+                .foregroundStyle(.tint)
+                .font(.largeTitle)
+                
+                Text("Tip Calculator")
+                    .font(.largeTitle)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color.gray)
+                    .multilineTextAlignment(.center)
+            }
+            HStack {
+                Text("Rs.")
+                TextField("Amount", text: $total)
+            }
+            HStack {
+                Slider(value: $tippersent, in: 1...50, step: 1.0)
+                Text("\(Int(tippersent))")
+                Text("%")
+            }
+            //in here first if statement is meant to check that textfiled is not empty or with white characters if its not continue with other two statements
+            if total.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                Text("Please add bill amount")
+            }else{
+                if let totalNum = Double(total) {
+                    Text("Tip amount is Rs.\(totalNum * tippersent / 100, specifier: "%0.2f")")
+                }else {
+                    Text("please add only numeric")
+                }
+            }
+                
         }
         .padding()
     }
