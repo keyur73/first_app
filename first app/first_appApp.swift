ef4c2c8a7ece7a1a4e0f44bab55be053c167c130
keyur73 <keyurkhambhadiya73@gmail.com>
2024-04-02 12:37:06 +0530

    Initial Commit

---
+++ first app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first app
+//
+//  Created by Keyur Khambhadiya on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
